53d0ce2eb29fc9e074c9402563f0ce56d844cf2f
Mwiza Kumwenda <Mwiza@miros-group.com>
2020-02-26 16:14:58 +0100

    Add http service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ConfigService } from './config.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'json-server-api';
+export class AppComponent implements OnInit {
+    public config;
+    title = 'json-server-api';
+    public currentDate = '';
+
+
+    ngOnInit(): void {
+       this.showConfig();
+    }
+
+    constructor(private configService: ConfigService) {}
+
+    showConfig() {
+        this.configService.getConfig()
+            .subscribe((data: any) => {
+            this.config = data;
+            console.log(`data: ${JSON.stringify(data.date)}`);
+            this.currentDate = JSON.stringify(data.date);
+            console.log(`c data : ${this.currentDate}`);
+
+      });
+  }
 }

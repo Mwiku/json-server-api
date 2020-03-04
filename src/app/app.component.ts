b70afb1975946950aecbaf1ec3b54de60ce5426c
Mwiza Kumwenda <Mwiza@miros-group.com>
2020-03-04 12:58:46 +0100

    Update api data and urls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ConfigService } from './config.service';
+import * as moment from 'moment-timezone';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +12,18 @@ export class AppComponent implements OnInit {
     title = 'json-server-api';
     public currentDate = '';
 
+    // jun = moment();
 
     ngOnInit(): void {
        this.showConfig();
     }
 
-    constructor(private configService: ConfigService) {}
+    constructor(private configService: ConfigService) {
+        let tid = moment.tz('Etc/GMT-1').format('YYYY-DD-MM, HH-mm-ss');
+        // this.jun.tz('America/Los_Angeles').format('HH:MM:ss a z');
+        console.log(tid);
+        // console.log(moment.tz.names());
+    }
 
     showConfig() {
         this.configService.getConfig()
--- src/app/config.service.ts
+++ src/app/config.service.ts
@@ -3,11 +3,14 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class ConfigService {
+    public latitude = -59.834541;
+    public longitude =  10.437390;
+
   constructor(private http: HttpClient) { }
 
   configUrl = 'http://localhost:14961/api/locationtime';
 
 getConfig() {
-        return this.http.get(this.configUrl);
+        return this.http.get(`${this.configUrl}/l/?latitude=${this.latitude}&longitude=${this.longitude}`);
     }
 }
